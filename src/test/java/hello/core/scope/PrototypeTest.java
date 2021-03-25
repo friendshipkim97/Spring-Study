7eecd8f0a3d0c7c4a69f7de846f42223e97f3aee
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-03-25 13:01:46 +0900

    final update

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,47 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class); // 이 때 새로운 객체가 생성됐기 때문에 init이 호출되는 것이다.
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2); // 프로토타입이라서 서로 다른 주소가 나온다.
+
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+        ac.close();
+
+    }
+
+    @Scope("prototype")
+    // @Component가 없어도 되는 이유는 스프링 컨테이너를 만들때 매개변수로 이 클래스를 넣었기 때문이다.
+    static class PrototypeBean{
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+
+}
