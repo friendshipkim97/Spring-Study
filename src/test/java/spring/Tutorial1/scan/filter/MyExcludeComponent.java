150e52a5b889a76f80c225fc3af5a383afa2be0a
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-06-23 18:29:58 +0900

    Add ExcludeComponent

---
+++ src/test/java/spring/Tutorial1/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,9 @@
+package spring.Tutorial1.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
